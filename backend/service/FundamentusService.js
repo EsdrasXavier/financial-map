629d780805e60c12070733b4e135dae4be2c654a
EsdrasXavier <esdrasxa@gmail.com>
2020-11-11 22:23:31 -0300

    Corrigido filtro de ações e alterada biblioteca de busca de dados.

--- backend/service/FundamentusService.js
+++ backend/service/FundamentusService.js
@@ -1,82 +1,76 @@
 'use strict';
 
 const FormData = require('form-data');
-const http = require('http');
 const HOST = 'www.fundamentus.com.br';
 const FUNDAMENTUS_URL = `http://${HOST}/resultado.php`;
 
-const FundamentusService = {
+const fetch = require('node-fetch');
+class FundamentusService {
+  static #LINK_REGEX = new RegExp(/<a [^>]+>(.*?)<\/a>/);
+  static #CELL_REGEX = new RegExp(/<td>(.*?)<\/td>/);
 
-  async fetchStocks() {
+  static async fetchStocks() {
     return new Promise((resolve, rejects) => {
+      var formdata = new FormData();
+      formdata.append("firma_ebit_min", "0");
+      formdata.append("firma_ebitda_min", "0");
+      formdata.append("liq_min", "200000");
+      formdata.append("ordem", "1");
 
-      const form = new FormData();
-      form.append('firma_ebit_min', 0);
-      form.append('firma_ebitda_min', 0);
-      form.append('liq_min', 200000);
-      form.append('negociada', 'ON');
-      form.append('ordem', 1);
-      form.append('x', 26);
-      form.append('y', 17);
-
-      const options = {
+      const requestOptions = {
         method: 'POST',
-        port: 80,
-        hostname: HOST,
-        path: FUNDAMENTUS_URL,
+        body: formdata,
+        redirect: 'follow',
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
-          'Content-Type': 'application/json; charset=utf-8',
-          'Cookie': 'PHPSESSID=543dfeb728a30a16429ba7af187aa864'
-        }
+          'Cookie': 'PHPSESSID=543dfeb728a30a16429ba7af187aa864',
+        },
       };
 
-      const request = http.request(options, (res) => {
-        res.setEncoding('utf-8');
-
-        let data = '';
-
-        res.on('data', chunk => data += chunk);
-
-        res.on('end', () => {
-          const linkRegex = new RegExp(/<a [^>]+>(.*?)<\/a>/);
-          const cellRegex = new RegExp(/<td>(.*?)<\/td>/);
-          const rows = data.split("\n");
-
-          const header = rows
-            .filter(line => line.includes('</th>'))
-            .map(line => (linkRegex.exec(line) || [])[1])
-            .filter(col => col != null);
-          const content = rows
-            .filter(line => line.includes('</td>'))
-            .map(line => (linkRegex.exec(line) || cellRegex.exec(line) || [])[1])
-            .filter(line => line != null);
-
-          const tableObj = { header, rows: [] };
-
-          let index = 0;
-          let obj = {};
-          content.forEach(row => {
-            obj[header[index++]] = row;
-
-            if (index >= header.length) {
-              index = 0;
-              tableObj.rows.push(obj);
-              obj = {};
-            }
-          });
-
-          resolve({ message: 'ok', data: tableObj });
-        });
-      });
-
-      request.on('error', error => {
-        console.error(error);
-        rejects(error);
-      });
-
-      form.pipe(request);
+      fetch(FUNDAMENTUS_URL, requestOptions)
+        .then(response => response.text())
+        .then(data => {
+          const tableData = FundamentusService.#parseData(data);
+          resolve({ message: 'ok', data: tableData });
+        })
+        .catch(error => rejects(error));
     });
+  }
+
+  static #parseData = data => {
+    const rows = data.split("\n");
+    const header = FundamentusService.#getTableHeader(rows);
+    const content = FundamentusService.#getTableContent(rows);
+
+    const tableObj = { header, rows: [] };
+
+    let index = 0;
+    let obj = {};
+    content.forEach(row => {
+      obj[header[index++]] = row;
+
+      if (index >= header.length) {
+        index = 0;
+        tableObj.rows.push(obj);
+        obj = {};
+      }
+    });
+
+    return tableObj;
+  }
+
+  static #getTableContent(rows) {
+    return rows
+      .filter(line => line.includes('</td>'))
+      .map(line => (FundamentusService.#LINK_REGEX.exec(line) || FundamentusService.#CELL_REGEX.exec(line) || [])[1])
+      .filter(line => line != null);
+  }
+
+  static #getTableHeader(rows) {
+    return rows
+      .filter(line => line.includes('</th>'))
+      .map(line => (FundamentusService.#LINK_REGEX.exec(line) || [])[1])
+      .filter(col => col != null);
   }
 }
 
