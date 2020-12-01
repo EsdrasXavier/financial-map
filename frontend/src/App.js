46c59234a0f5db0693f11a593edde791a6507bc8
Esdras Xavier <esdrasxa@gmail.com>
2020-12-01 20:41:35 -0300

    Criada integração com TOP ações.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Layout } from 'antd';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './app.css';
+import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
 import Stocks from './components/Stocks/Stocks';
--- frontend/src/components/Stocks/Stocks.jsx
+++ frontend/src/components/Stocks/Stocks.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import { Col, Row, Table } from 'antd';
+import React, { useEffect, useState } from 'react';
 
 const Stocks = () => {
+  const [header, setHeader] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch('http://127.0.0.1:3000/stock')
+      .then(response => response.json())
+      .then(responseData => {
+        const columns = responseData['data']['header'].map(column => ({
+          title: column,
+          dataIndex: column,
+          key: column,
+        }));
+
+        setHeader(columns);
+        setData(responseData['data']['rows']);
+      })
+      .catch(error => console.error(error));
+  }
 
   return (
     <div>
-      <h1>
-        Stocks
-      </h1>
+      <div className="stock-title">
+        <h1>
+          Top Ações
+        </h1>
+      </div>
+      <Row>
+        <Col span={24}>
+          <Table
+            total={50}
+            className="top-ten-stocks"
+            size="large"
+            columns={header}
+            bordered
+            dataSource={data}
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
