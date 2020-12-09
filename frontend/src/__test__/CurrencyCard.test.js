aa49fedfe79ceacd1eddce6f327b7543f21b4b47
weivenotto <49216750+weivenotto@users.noreply.github.com>
2020-12-08 21:44:39 -0300

    novos parametros no currencycard

--- frontend/src/__test__/CurrencyCard.test.js
+++ frontend/src/__test__/CurrencyCard.test.js
@@ -4,8 +4,23 @@ import CurrencyCard from '../components/CurrencyCard/CurrencyCard';
 describe('Test the `<CurrencyCard />` component.', () => {
 
   test('Renders the app and check if the title is loaded.', () => {
-    render(<CurrencyCard name={"Dolar"} price={"5,32"} />);
+    render(<CurrencyCard name={"Dolar"} price={"5.1203"} />);
     const linkElement = screen.getByText(/Dolar/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Renders the app and check if the title is loaded.', () => {
+    render(<CurrencyCard name={"Euro"} price={"6.2073"} />);
+    const linkElement = screen.getByText(/Euro/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Renders the app and check if the title is loaded.', () => {
+    render(<CurrencyCard name={"Argentine Peso"} price={"0.0627"} />);
+    const linkElement = screen.getByText(/Argentine Peso/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Renders the app and check if the title is loaded.', () => {
+    render(<CurrencyCard name={"Pound Sterling"} price={"6.8493"} />);
+    const linkElement = screen.getByText(/Pound Sterling/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
