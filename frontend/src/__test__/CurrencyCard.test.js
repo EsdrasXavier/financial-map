c3e93d3b97d688c8d7602c8dbc19e36d42aedc02
weivenotto <49216750+weivenotto@users.noreply.github.com>
2020-12-08 21:07:55 -0300

    adicionando novo cenario de teste

---
+++ frontend/src/__test__/CurrencyCard.test.js
@@ -0,0 +1,11 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import CurrencyCard from '../components/CurrencyCard/CurrencyCard';
+
+describe('Test the `<CurrencyCard />` component.', () => {
+
+  test('Renders the app and check if the title is loaded.', () => {
+    render(<CurrencyCard name={"Dolar"} price={"5,32"} />);
+    const linkElement = screen.getByText(/Dolar/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
