eccb4797683d25aa797719ba49f2c24364743737
Thiago Franzener <thiago@movtech.inf.br>
2020-12-08 21:21:24 -0300

    Tela dashboard
    Finalização do desenvolvimento da tela de Dashboard.

--- frontend/src/components/Dashboard/Currencies/Currencies.jsx
+++ frontend/src/components/Dashboard/Currencies/Currencies.jsx
@@ -1,19 +1,35 @@
-import { Col, Divider, Row } from 'antd';
-import React from 'react';
+import { Col, Divider, Row, notification } from 'antd';
+import React, { useEffect, useState } from 'react';
 import CurrencyCard from '../../CurrencyCard/CurrencyCard';
 
-const Currencies = () => {
+const openNotification = error => {
+  notification['error']({
+    message: 'Erro ao buscar dados',
+    description: `Erro ao ler ações: ${error.message}`
+  });
+};
 
-  const currencies = [
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-    { name: 'Dolar', price: '$5,32' },
-  ];
+const Currencies = () => {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch('http://127.0.0.1:3000/currencies')
+      .then(response => response.json())
+      .then(responseData => {
+        const obj = Object.keys(responseData['data']);
+        let dataCurrencies = [];
+        for(let i in obj) {
+          if (obj[i] != 'source'){
+            dataCurrencies.push({name: responseData['data'][obj[i]].name, price: responseData['data'][obj[i]].buy});
+          }
+        }
+        setCurrencies(dataCurrencies);
+      }).catch(openNotification);
+  }
 
   return (
     <div>
